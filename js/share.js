366a94ff79d20154a947e2909c9f0925987300f3
kevinorellana <alexanderorellana867@gmail.com>
2022-02-25 19:13:02 +0100

    Hemos estilizado el mensaje de compartir y le hemos dado interactividad

--- js/share.js
+++ js/share.js
@@ -1,17 +1,15 @@
 var sharebtn = document.getElementById('share');
 var cerrar = document.getElementById('cerrar');
 
-if (compartir.style.display = "none") {
     sharebtn.addEventListener('click',mostrar);
-}else {
+
     cerrar.addEventListener('click',ocultar);
-}
 
 function mostrar() {
     var compartir = document.getElementById('compartir');
-    compartir.style.display = "block";
+    compartir.classList.add('active');
 }
 function ocultar() {
     var compartir = document.getElementById('compartir');
-    compartir.style.display = null;
+    compartir.classList.remove('active');
 }
